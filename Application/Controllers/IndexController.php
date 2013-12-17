84b006db1e3387d3afa9a9a9f21018b15e202e3d
wesley1263 <webdesigner.ibc@gmail.com>
2013-12-17 10:58:28 -0200

    New library PHPMail

--- Application/Controllers/IndexController.php
+++ Application/Controllers/IndexController.php
@@ -4,6 +4,7 @@ use Application\Models\Teste;
 use Vendor\Library\Pagination\Pagination;
 use Vendor\Library\Language\Language;
 use Vendor\Library\Cache\Cache;
+use Vendor\Library\PHPMailer\PHPMailer;
 
 
 class IndexController extends OXE_Controller {
@@ -78,6 +79,34 @@ class IndexController extends OXE_Controller {
 			echo $cache->readCache('correios');
 		}else{
 			echo $result->CalcPrecoPrazoResult->Servicos->cServico->MsgErro;
+		}
+	}
+	
+	public function mailAction()
+	{
+		//Create a new PHPMailer instance
+		$mail = new PHPMailer();
+		//Set who the message is to be sent from
+		$mail->setFrom('from@example.com', 'First Last');
+		//Set an alternative reply-to address
+		$mail->addReplyTo('replyto@example.com', 'First Last');
+		//Set who the message is to be sent to
+		$mail->addAddress('whoto@example.com', 'John Doe');
+		//Set the subject line
+		$mail->Subject = 'PHPMailer mail() test';
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		$mail->msgHTML('Hello world');
+		//Replace the plain text body with one created manually
+		$mail->AltBody = 'This is a plain-text message body';
+		//Attach an image file
+		$mail->addAttachment('images/phpmailer_mini.gif');
+		
+		//send the message, check for errors
+		if (!$mail->send()) {
+		    echo "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+		    echo "Message sent!";
 		}
 	}
 
